1319def0db65f63461d3d3a8d2bea08f0657092c
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-09-08 00:11:44 +0800

    📦 utils: remove dummy function

--- src/utils/dummyFunction.ts
+++
@@ -1,7 +0,0 @@
-export const dummyFunction = (value: number, asString: boolean = false) => {
-	return asString ? value.toString() : value;
-};
-
-export const untestedFunction = () => {
-	return '';
-};
