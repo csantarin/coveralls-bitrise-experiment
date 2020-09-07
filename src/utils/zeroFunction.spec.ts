dad5bfebff0689467497d1d77dec317413956baf
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-09-08 00:19:24 +0800

    ✨ utils: reduce coverage score

--- src/utils/zeroFunction.spec.ts
+++ src/utils/zeroFunction.spec.ts
@@ -1,7 +1,13 @@
-import zeroFunction from './zeroFunction';
+import zeroFunction, { returnZero } from './zeroFunction';
 
 describe('zeroFunction', () => {
 	test('should exist', () => {
 		expect(zeroFunction).toBeDefined();
+	});
+});
+
+describe('returnZero', () => {
+	test('should exist', () => {
+		expect(returnZero).toBeDefined();
 	});
 });
--- src/utils/zeroFunction.ts
+++ src/utils/zeroFunction.ts
@@ -1,3 +1,7 @@
 export default function zeroFunction() {
 	return 0;
 }
+
+export const oneFunction = () => {
+	return 1;
+};
