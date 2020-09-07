1366258569031ea820a71f2f1c3935b1f5b7ec20
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-09-07 23:56:04 +0800

    ✨ utils: reduce coverage score

--- src/utils/dummyFunction.spec.ts
+++ src/utils/dummyFunction.spec.ts
@@ -4,11 +4,4 @@ describe('dummyFunction', () => {
 	it('should exist', () => {
 		expect(dummyFunction).toBeDefined();
 	});
-
-	it('should spit out the value according to the setting', () => {
-		expect(dummyFunction(123)).toBe(123);
-		expect(dummyFunction(123, undefined)).toBe(123);
-		expect(dummyFunction(123, false)).toBe(123);
-		expect(dummyFunction(123, true)).toBe('123');
-	});
 });
--- src/utils/dummyFunction.ts
+++ src/utils/dummyFunction.ts
@@ -1,3 +1,7 @@
 export const dummyFunction = (value: number, asString: boolean = false) => {
 	return asString ? value.toString() : value;
 };
+
+export const untestedFunction = () => {
+	return '';
+};
